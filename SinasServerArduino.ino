15c7cf71d96deaaae1708243bc6d65c97ac43cd0
Henri Kim Pinheiro Rodrigues <hkimpr40@gmail.com>
2025-04-29 23:16:40 -0300

    coment sketch and Controller code

--- SinasServerArduino.ino
+++ SinasServerArduino.ino
@@ -1,60 +1,65 @@
-int pulsesPerRotation = 1;
-int minRpm = 450;
-int maxRpm = 10000;
-int maxPulseWidth = 60000/minRpm;
-int minPulseWidth = 60000/maxRpm;
+// set the initial values
+int pulsesPerRotation = 1; // 1 pulse per rotation
+int minRpm = 450; // 450 RPM
+int maxRpm = 10000; // 10000 RPM
+int maxPulseWidth = 60000/minRpm; // set the maximum pulse width
+int minPulseWidth = 60000/maxRpm; // set the minimum pulse width
 
 volatile unsigned long pulseCount = 0;
 volatile unsigned long lastPulseTime = 0;
 
-void onPulse() {
-    if(millis()-lastPulseTime < minPulseWidth) return;
+void onPulse() { // interrupt handler for fan pulse counting
+    if(millis()-lastPulseTime < minPulseWidth) return; // ignore pulses that are too long
     pulseCount++;
     lastPulseTime = millis();
 }
 
 void setup() {
-    Serial.begin(57600);
-    pinMode(5, OUTPUT);
-    pinMode(3, INPUT);
-    pinMode(13, OUTPUT);
-    attachInterrupt(digitalPinToInterrupt(3), onPulse, RISING);
+    Serial.begin(57600); // setup the serial port with 57600 baud rate
+    pinMode(5, OUTPUT); // setup the PWM pin
+    pinMode(3, INPUT); // setup the interrupt pin
+    pinMode(13, OUTPUT); // setup the buildin LED pin
+    attachInterrupt(digitalPinToInterrupt(3), onPulse, RISING); // attach the interrupt to pin 3
     digitalWrite(13, 1);
 }
 
 void loop() {
-    if(Serial.available() > 0) {
-        digitalWrite(13, 0);
-        char dataReceived = Serial.read();
+    if(Serial.available() > 0) { // check for available data
+        digitalWrite(13, 0); // turn off the buildin led to indicate data is being received
+        char dataReceived = Serial.read(); // get the byte
 
-        if(dataReceived == 0x00) {
+        // check the command received
+        if(dataReceived == 0x00) { // get the RPM
             unsigned long rpm = 0;
 
-            noInterrupts();
+            noInterrupts(); // disable interrupts to reset the pulse count
             pulseCount = 0;
-            interrupts();
-            delay(maxPulseWidth);
-            noInterrupts();
-            rpm += pulseCount*pulsesPerRotation*(60000/maxPulseWidth);
-            interrupts();
+            interrupts(); // enable interrupts again to start counting pulses
+            delay(maxPulseWidth); // wait for the maximum pulse width to count the pulses
+            noInterrupts(); // disable interrupts to read the pulse count
+            rpm += pulseCount*pulsesPerRotation*(60000/maxPulseWidth); // calculate the RPM
+            interrupts(); // enable interrupts again
             
-            Serial.write(0x02);
+            // send the RPM response
+            Serial.write(0x02); // len
             Serial.write(lowByte(rpm));
             Serial.write(highByte(rpm));
-            Serial.flush();
+            Serial.flush(); // send the data
         }
-        else if((dataReceived & 0x80) != 0) {
-            uint8_t pwmValue = (dataReceived & 0x7f)*2;
-            analogWrite(5, pwmValue);
+        else if((dataReceived & 0x80) != 0) { // if the command has the MSB 1 (1000 0000), the receive the first 7bits to set the PWM
+            uint8_t pwmValue = (dataReceived & 0x7f)*2; // get the first 7 bits and multiply by 2 to get the PWM value
+            analogWrite(5, pwmValue); // set the PWM value
+
+            // send some response
             Serial.write(0x01);
             Serial.write(dataReceived);
             Serial.flush();
         }
-        else {
+        else { // if the command isn't registred, send the command back
             Serial.write(0x01);
             Serial.write(dataReceived);
             Serial.flush();
         }
-        digitalWrite(13, 1);
+        digitalWrite(13, 1); // turn on the buildin led to indicate that the communication is done
     }
 }
--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -9,33 +9,35 @@
 
 using namespace std;
 
+// define functions
 void help();
 
 int configTTY(struct termios *tty, int arduinoStream);
 
 int main(int argc, char* argv[]) {
-    if(argc < 3) {
-        help();
+    if(argc < 3) { // check the number of arguments
+        help(); // print help if not enough arguments
         return 0;
     }
-    char* portName = argv[1];
-    int arduinoStream = open(portName, O_RDWR | O_NOCTTY);
 
-    if(arduinoStream == -1) {
+    char* portName = argv[1]; // get the port name from the arguments
+    int arduinoStream = open(portName, O_RDWR | O_NOCTTY); // open the tty port stream
+
+    if(arduinoStream == -1) { // check if the port is opened
         cerr << "Can't open \"" << portName << "\" port.\n";
         return 0;
     } 
 
-    struct termios tty;
-    if(configTTY(&tty, arduinoStream) < 0) {
+    struct termios tty; // create a termios struct to configure the tty port
+    if(configTTY(&tty, arduinoStream) < 0) { // check if the port is configured
         cerr << "Error setting serial port.\n";
     }
 
-    char dataToSend = stoi(argv[2], nullptr, 16);
+    char dataToSend = stoi(argv[2], nullptr, 16); // convert the command to a char
 
-    ssize_t bytesWritten = write(arduinoStream, &dataToSend, 1);
+    ssize_t bytesWritten = write(arduinoStream, &dataToSend, 1); // send the command to the arduino
 
-    if(bytesWritten < 0) {
+    if(bytesWritten < 0) { // check if the command is sent
         cerr << "Error sending data to \"" << portName << "\" port.\n";
     }
 
@@ -44,53 +46,56 @@ int main(int argc, char* argv[]) {
     uint8_t packageSize;
     uint8_t byteReceived;
 
-    while(1) {
-        ssize_t bytesReads = read(arduinoStream, &packageSize, 1);
+    while(1) { // wait for the len response
+        ssize_t bytesReads = read(arduinoStream, &packageSize, 1); // read the last byte
         
-        if(bytesReads > 0) {
+        if(bytesReads > 0) { // check if the byte is read and then close the loop
             cout << "\033[0;32m>>\033[0m 0x" << hex << setw(2) << setfill('0') << (packageSize & 0xFF) << "\n";
             break;
         }
-        if(bytesReads < 0) {
+        if(bytesReads < 0) { // if there is an error
             cerr << "\033[0;31m>>\033[0m " << "\033[1;31mERROR\033[0m\nError: " << strerror(errno) << ".\n";
         }
+        // if the bytesize is 0 try again
     }
 
-    for(int i=0;i<packageSize;i++) {
-        ssize_t bytesReads = read(arduinoStream, &byteReceived, 1);
+    for(int i=0;i<packageSize;i++) { // read the rest of the package 
+        ssize_t bytesReads = read(arduinoStream, &byteReceived, 1); // read the byte
 
-        if(bytesReads > 0) {
+        if(bytesReads > 0) { // check if the byte is read
             cout << "\033[0;36m>>\033[0m 0x" << hex << setw(2) << setfill('0') << (byteReceived & 0xFF) << "\n";
         }
-        if(bytesReads < 0) {
+        if(bytesReads < 0) { // if there is an error
             cerr << "\033[0;31m>>\033[0m " << "ERROR\nError: " << strerror(errno) << ".\n";
         }
+        // if the bytesize is 0 passes
     }
 
-    close(arduinoStream);
+    close(arduinoStream); // close the port stream
+    return 0;
 }
 
-void help() {
+void help() { // show the help message
     cout << "Use: ./program <devide> <command>\nExemple: ./program /dev/ttyUSB0 45\n";
 }
 
-int configTTY(struct termios *tty, int arduinoStream) {
-    memset(tty, 0, sizeof(*tty));
-    if(tcgetattr(arduinoStream, tty) != 0) return -1;
+int configTTY(struct termios *tty, int arduinoStream) { // configure the tty port
+    memset(tty, 0, sizeof(*tty)); // clear the struct
+    if(tcgetattr(arduinoStream, tty) != 0) return -1; // get the current attributes
 
-    cfsetispeed(tty, B57600);
-    cfsetospeed(tty, B57600);
+    cfsetispeed(tty, B57600); // set the input speed
+    cfsetospeed(tty, B57600); // set the output speed
 
-    tty->c_cflag &= ~PARENB;
-    tty->c_cflag &= ~CSTOPB;
-    tty->c_cflag &= ~CSIZE;
-    tty->c_cflag |= CS8;
-    tty->c_cflag |= CREAD | CLOCAL;
-    tty->c_cflag &= ~HUPCL;
-    tty->c_lflag &= ~(ICANON | ECHO | ECHOE | ECHONL | ISIG);
-    tty->c_iflag &= ~(IXON | IXOFF | IXANY | IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
+    tty->c_cflag &= ~PARENB; // disable parity
+    tty->c_cflag &= ~CSTOPB; // set 1 stop bit
+    tty->c_cflag &= ~CSIZE; // clear the size bits
+    tty->c_cflag |= CS8; // set 8 data bits
+    tty->c_cflag |= CREAD | CLOCAL; // enable receiver and ignore modem control lines
+    tty->c_cflag &= ~HUPCL; // disable hang up on close
+    tty->c_lflag &= ~(ICANON | ECHO | ECHOE | ECHONL | ISIG); // disable canonical mode, echo, and signal characters
+    tty->c_iflag &= ~(IXON | IXOFF | IXANY | IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL); // disable input flags
 
-    if(tcsetattr(arduinoStream, TCSANOW, tty) != 0) return -1;
+    if(tcsetattr(arduinoStream, TCSANOW, tty) != 0) return -1; // set the attributes
 
     return 0;
 }
